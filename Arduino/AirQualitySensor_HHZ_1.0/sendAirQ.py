fdac2bf94ca1b7aba33339c3f47d3cf3dfad0ff8
manh3141 <manh3141@users.noreply.github.com>
2018-01-29 14:22:07 +0100

    Add files via upload
    Upload files for the air quality sensor

---
+++ Arduino/AirQualitySensor_HHZ_1.0/sendAirQ.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+# Import required modules
+import os
+import time
+import paho.mqtt.client as mqtt
+
+
+# Define credentials and connection details of MQTT broker
+user = "digitalhhz"
+passwd = "hackathon"
+mqttbroker = "192.168.0.12"
+channel = "hhz/126_2/4/1/1/0/1"
+
+# Define path of the airsensor-script and command to be executed
+dir = "/home/hhz/Scripts/airsensor"
+cmd = dir + " -v -o"
+
+# Execute airsensor-script to get air quality value
+valAirQ = ""
+p = os.popen(cmd,"r")
+
+while 1:
+    # Save air quality value to a variable
+    valAirQ = p.readline()
+    if not valAirQ: break
+    valAirQ = valAirQ.replace("\n", "")    
+
+    # Send air quality value to MQTT broker
+    def on_connect(client, userdata, flags, rc):
+      print("Connected with result code " + str(rc))
+
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.username_pw_set(user, passwd)
+    client.connect(mqttbroker, 1883, 60)
+    client.publish(channel, valAirQ)
+
