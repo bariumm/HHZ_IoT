69b04457c58306e83df2aae6a789d0b45f471a79
manh3141 <manh3141@users.noreply.github.com>
2018-01-29 14:17:45 +0100

    Add files via upload
    Upload of new Arduino sketches (private)

---
+++ Arduino/HumidityTemperature_HHZ_v.1.0/HumidityTemperature_HHZ_v.1.0.ino
@@ -0,0 +1,168 @@
+/**
+ * The MySensors Arduino library handles the wireless radio link and protocol
+ * between your home built sensors/actuators and HA controller of choice.
+ * The sensors forms a self healing radio network with optional repeaters. Each
+ * repeater and gateway builds a routing tables in EEPROM which keeps track of the
+ * network topology allowing messages to be routed to nodes.
+ *
+ * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
+ * Copyright (C) 2013-2015 Sensnology AB
+ * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
+ *
+ * Documentation: http://www.mysensors.org
+ * Support Forum: http://forum.mysensors.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ *******************************
+ *
+ * REVISION HISTORY
+ * Version 1.0: Henrik EKblad
+ * Version 1.1 - 2016-07-20: Converted to MySensors v2.0 and added various improvements - Torben Woltjen (mozzbozz)
+ * 
+ * DESCRIPTION
+ * This sketch provides an example of how to implement a humidity/temperature
+ * sensor using a DHT11/DHT-22.
+ *  
+ * For more information, please visit:
+ * http://www.mysensors.org/build/humidity
+ * 
+ */
+
+/******************************************************************* 
+ * EDIT: June 06, 2017
+ * DESCRIPTION 
+ * As part of an one-year project at Herman Hollerith Zentrum (HHZ),
+ * an IoT environment was set up. The original script was modified 
+ * to integrate the sensor into our IoT environment.
+ * 
+ * ADDITIONAL INFORMATION
+ * -  Used sensor: DHT-11 (combined humidity and temperature sensor)
+ * -  Cabling: NC (PIN3) was left out
+ * -  Integrate the DHT library from https://github.com/mysensors/MySensorsArduinoExamples/archive/master.zip
+ *    before compiling this script
+ * - EG-120 uses channel 0 -> 2.400 GHz
+ * - EG-125 uses channel 1 -> 2.401 GHz
+ * - EG-126 uses channel 2 -> 2.402 GHz
+ * 
+ *  Modified by manh3141 
+ */
+
+// Enable debug prints
+#define MY_DEBUG
+
+// Enable and select radio type attached 
+#define MY_RADIO_NRF24
+//#define MY_RADIO_RFM69
+//#define MY_RS485
+
+// Define individual constants
+#define MY_RF24_CHANNEL  0 // Channel
+#define MY_NODE_ID 2 // Node id
+char MY_SENSOR_METADATA[] = "HHZ EG-120 HumidTemp_A"; // Friendly-name of the sensor (replace _TAG_ with your individual tag and _LOC_ with the location of the sensor)
+char MY_SKETCH_VERSION[] = "1.0"; // Sketch version
+
+// Include required libraries
+#include <SPI.h>
+#include <MySensors.h>  
+#include <DHT.h>
+
+// Set this to the pin you connected the DHT's data pin to
+#define DHT_DATA_PIN 3
+
+// Set this offset if the sensor has a permanent small offset to the real temperatures
+#define SENSOR_TEMP_OFFSET 0
+
+// Sleep time between sensor updates (in milliseconds)
+// Must be >1000ms for DHT22 and >2000ms for DHT11
+static const uint64_t UPDATE_INTERVAL = 600000; // 10 minute sleep time between reports (in milliseconds)
+
+// Force sending an update of the temperature after n sensor reads, so a controller showing the
+// timestamp of the last update doesn't show something like 3 hours in the unlikely case, that
+// the value didn't change since;
+// i.e. the sensor would force sending an update every UPDATE_INTERVAL*FORCE_UPDATE_N_READS [ms]
+static const uint8_t FORCE_UPDATE_N_READS = 10;
+
+#define CHILD_ID_HUM 0
+#define CHILD_ID_TEMP 1
+
+bool metric = true;
+
+MyMessage msgHum(CHILD_ID_HUM, V_HUM);
+MyMessage msgTemp(CHILD_ID_TEMP, V_TEMP);
+DHT dht;
+
+
+void presentation()  
+{ 
+  // Send the sketch version information to the gateway
+  sendSketchInfo(MY_SENSOR_METADATA, MY_SKETCH_VERSION); // Send the sketch version information to the gateway and controller
+  
+  // Register all sensors to gw (they will be created as child devices)
+  present(CHILD_ID_HUM, S_HUM);
+  present(CHILD_ID_TEMP, S_TEMP);
+  
+  metric = getControllerConfig().isMetric;
+}
+
+
+void setup()
+{
+  dht.setup(DHT_DATA_PIN); // set data pin of DHT sensor
+  if (UPDATE_INTERVAL <= dht.getMinimumSamplingPeriod()) {
+    Serial.println("Warning: UPDATE_INTERVAL is smaller than supported by the sensor!");
+  }
+  // Sleep for the time of the minimum sampling period to give the sensor time to power up
+  // (otherwise, timeout errors might occure for the first reading)
+  sleep(dht.getMinimumSamplingPeriod());
+}
+
+
+void loop()      
+{  
+  // Force reading sensor, so it works also after sleep()
+  dht.readSensor(true);
+
+  Serial.println("Sensor:");
+  Serial.println(MY_SENSOR_METADATA);
+  Serial.println("Node ID:");
+  Serial.println(MY_NODE_ID);
+  Serial.println("Channel:");
+  Serial.println(MY_RF24_CHANNEL);
+  
+  // Get temperature from DHT library
+  float temperature = dht.getTemperature();
+  if (isnan(temperature)) {
+    Serial.println("Failed reading temperature from DHT!");
+  } else {
+    
+    if (!metric) {
+      temperature = dht.toFahrenheit(temperature);
+    }
+    
+    send(msgTemp.set(temperature, 1));
+
+    #ifdef MY_DEBUG
+    Serial.print("T: ");
+    Serial.println(temperature);
+    #endif
+  } 
+
+  // Get humidity from DHT library
+  float humidity = dht.getHumidity();
+  if (isnan(humidity)) {
+    Serial.println("Failed reading humidity from DHT");
+  } else {
+    send(msgHum.set(humidity, 1));
+    
+    #ifdef MY_DEBUG
+    Serial.print("H: ");
+    Serial.println(humidity);
+    #endif
+  }
+
+  // Sleep for a while to save energy
+  sleep(UPDATE_INTERVAL); 
+}
